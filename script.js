e770676ea327b05709c9e3ba4146a31dadb01e6f
Drew Davis <drew@davisdre.com>
2021-08-08 21:52:56 -0400

    Added a city search function.

--- script.js
+++ script.js
@@ -1,25 +1,30 @@
 var token = config.MY_API_TOKEN;
-var button = document.querySelector('.button')
-var inputValue = document.querySelector('.inputValue')
-var name = document.querySelector('.name')
-var temp = document.querySelector('.temp')
-var weather = document.querySelector('.weather')
-var icon = document.querySelector('.icon')
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var icon = document.querySelector('.icon');
+var button= document.querySelector('.submit');
 
-button.addEventListener("click", function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&units=imperial&appid=' + token)
-    .then(response => response.json())
-    .then(data => {
-        var iconValue="https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
-        var tempValue= Math.floor(data.main.temp);
-        var weatherValue=data.weather[0].main;
-        var nameValue=data.name;
 
-        name.innerHTML = nameValue;
-        temp.innerHTML = tempValue;
-        weather.innerHTML = weatherValue;
-        icon.innerHTML = iconValue;
-    })
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid='+token)
+.then(response => response.json())
+.then(data => {
+  var tempValue = Math.floor(data.main.temp);
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
 
-.catch(err => alert("Wrong city name!"))
+  
+  var iconValue =  data.weather[0].icon
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  icon.innerHTML = "<img src= 'https://openweathermap.org/img/w/" + iconValue + ".png'>";
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
 })
